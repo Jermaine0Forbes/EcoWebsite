6004aa1bdec558d1049f488f6b9c8cd9311af675
Jermaine Forbes <jermaine0forbes@gmail.com>
2020-09-17 02:21:39 +0000

    created: header, pages components; added: animations and css files

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PagesComponent } from './pages/pages.component';
+import { HeaderComponent } from './header/header.component';
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"pages", component:PagesComponent, data:{animation:"isLeft"}},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RouteComps = [AppComponent,HomeComponent, PagesComponent, HeaderComponent];
